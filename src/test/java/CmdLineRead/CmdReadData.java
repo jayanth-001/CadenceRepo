2bbaf58ead42bb65df388c56e31f7fe1807b7717
Lenovo <Lenovo@DESKTOP-01H70DH>
2022-03-16 11:46:36 +0530

    CmdLine...>>url fetching commit

---
+++ src/test/java/CmdLineRead/CmdReadData.java
@@ -0,0 +1,19 @@
+package CmdLineRead;
+
+import org.testng.annotations.Test;
+
+public class CmdReadData {
+	@Test
+	public void read()
+	{
+		String URL = System.getProperty("url");
+		System.out.println(URL);
+		System.out.println("done");
+		
+	}
+	
+	
+	
+	
+	
+}
